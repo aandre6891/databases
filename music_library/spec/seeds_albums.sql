1987cb30a3369cbe12bf5f93c9f29503b04cb8dd
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-24 18:47:49 +0100

    correct seeds_albums

--- music_library/spec/seeds_albums.sql
+++ music_library/spec/seeds_albums.sql
@@ -10,4 +10,4 @@ TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
 -- Replace these statements with your own seed data.
 
 INSERT INTO albums (title, release_year, artist_id) VALUES ( 'Doolittle', '1989', '1' );
-INSERT INTO albums (title, release_year, artist_id) VALUES ( 'Waterloo', '1974', '1974' );
+INSERT INTO albums (title, release_year, artist_id) VALUES ( 'Waterloo', '1974', '2' );
