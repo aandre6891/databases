7cc9c72740607683af66091fc356bd11e22496eb
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-25 17:12:37 +0100

    add test #find

--- music_library/spec/albums_repository_spec.rb
+++ music_library/spec/albums_repository_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe AlbumsRepository do
       reset_albums_table
     end
     
-    it "" do
+    it "returns number of albums and details of the first album" do
       repo = AlbumsRepository.new
 
       albums = repo.all
@@ -24,5 +24,29 @@ RSpec.describe AlbumsRepository do
       expect(albums[1].release_year).to eq "1974"
       expect(albums[1].artist_id).to eq "2"
     end
+
+    it "returns the first album 'Doolittle' details" do
+          # 2
+      # Get the first album 'Doolittle'
+
+      repo = AlbumsRepository.new
+      albums = repo.find(1)
+
+      albums.title # => 'Doolittle'
+      albums.release_year # => '1989'
+      albums.artist_id # => '1'
+    end
+    
+    # xit "returns the second album 'Baltimore' details"
+    #   # 3
+    #   # get the album Baltimore
+    
+    #   repo = AlbumsRepository.new
+    #   albums = repo.find(9)
+    
+    #   albums.title # => 'Baltimore'
+    #   albums.release_year # => '1978'
+    #   albums.artist_id # => '4'
+    # end
   end
 end
