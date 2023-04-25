c703a0c41ae45995a5f5751223175d303ef2b751
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-25 11:01:13 +0100

    amend seeds for book_store_test

--- book_store/spec/seeds_book_store.sql
+++ book_store/spec/seeds_book_store.sql
@@ -1,20 +1,16 @@
-DROP TABLE IF EXISTS "public"."books";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
+-- EXAMPLE
+-- (file: spec/seeds_{table_name}.sql)
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS books_id_seq;
+-- Write your SQL seed here. 
 
--- Table Definition
-CREATE TABLE "public"."books" (
-    "id" SERIAL,
-    "title" text,
-    "author_name" text,
-    PRIMARY KEY ("id")
-);
+-- First, you'd need to truncate the table - this is so our table is emptied between each test run,
+-- so we can start with a fresh state.
+-- (RESTART IDENTITY resets the primary key)
 
-INSERT INTO "public"."books" ("id", "title", "author_name") VALUES
-(1, 'Nineteen Eighty-Four', 'George Orwell'),
-(2, 'Mrs Dalloway', 'Virginia Woolf'),
-(3, 'Emma', 'Jane Austen'),
-(4, 'Dracula', 'Bram Stoker'),
-(5, 'The Age of Innocence', 'Edith Wharton');
+TRUNCATE TABLE books RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO books (title, author_name) VALUES ('Pride & Prejudice', 'Jane Austen');
+INSERT INTO books (title, author_name) VALUES ('The count of Montecristo', 'Alexandre Dumas');
