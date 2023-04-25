b6221d6e0cd575c192f9e5e32d0a9c7c48970e05
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-25 11:21:41 +0100

    move and amend book store repository spec

---
+++ book_store/spec/book_store_repository_spec.rb
@@ -0,0 +1,29 @@
+require "book_store_repositor"
+
+RSpec.describe BookStoreRepository do
+  describe BookStoreRepository do
+    def reset_books_table
+      seed_sql = File.read('spec/seeds_book_store.sql')
+      connection = PG.connect({ host: '127.0.0.1', dbname: 'book_store' })
+      connection.exec(seed_sql)
+    end
+
+    before(:each) do 
+      reset_books_table
+    end
+
+    it "" do
+      repo = BookStoreRepository.new
+
+      books = repo.all
+      
+      expect(books.length).to eq 2
+      expect(books[0].id).to eq "1"
+      expect(books[0].title).to eq 'Pride & Prejudice'
+      expect(books[0].author_name).to eq 'Jane Austin'
+      expect(books[1].id).to eq 2
+      expect(books[1].title).to eq 'The count of Montecristo'
+      expect(books[1].author_name).to eq 'Alexandre Dumas'
+    end
+  end
+end
