e4d1c48ab89cb191de585be89a8ab7179c7a6b2b
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-27 13:54:06 +0100

    add and complete seeds_users

---
+++ social_network/spec/seeds_users.sql
@@ -0,0 +1,9 @@
+TRUNCATE TABLE users RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO users (name, email_address) VALUES ('andy6891', 'andy6891@gmail.com');
+INSERT INTO users (name, email_address) VALUES ('john325', 'john325@hotmail.com');
+INSERT INTO users (name, email_address) VALUES ('lisa678', 'lisa@hotmail.com');
+INSERT INTO users (name, email_address) VALUES ('matt777', 'matt777@hotmail.com');
