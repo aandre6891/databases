5a562f3c598b60387f4911f0ab3cf1a3eaf2b425
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-28 16:14:41 +0100

    amend app.rb

--- music_library/app.rb
+++ music_library/app.rb
@@ -1,15 +1,27 @@
-# file: app.rb
 
 require_relative 'lib/database_connection'
 require_relative 'lib/artists_repository'
 require_relative 'lib/albums_repository'
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
-
 class Application
-  # def initialize(terminal)
-  #   @terminal = terminal
-  # end
+  def initialize(database_name, io, album_repository, artist_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @album_repository = album_repository
+    @artist_repository = artist_repository
+  end
+
+  def run
+    @io.puts "Please, select:" 
+    @io.puts "1. Print the artists"
+    @io.puts "2. Print the albums."
+    selection = @io.gets.chomp
+    if selection == '1'
+      print_artists
+    elsif selection == '2'
+      print_albums
+    end
+  end
+
   def print_artists
     artists_repository = ArtistsRepository.new
     
@@ -28,18 +40,17 @@ class Application
     end
   end
 
-  def run
-    puts "Please, select:" 
-    puts "1. Print the artists"
-    puts "2. Print the albums."
-    selection = gets.chomp
-    if selection == '1'
-      print_artists
-    elsif selection == '2'
-      print_albums
-    end
+# Don't worry too much about this if statement. It is basically saying "only
+# run the following code if this is the main file being run, instead of having
+# been required or loaded by another file.
+# If you want to learn more about __FILE__ and $0, see here: https://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Variables_and_Constants#Pre-defined_Variables
+  if __FILE__ == $0
+    app = Application.new(
+      'music_library',
+      Kernel,
+      AlbumsRepository.new,
+      ArtistsRepository.new
+    )
+    app.run
   end
 end
-
-application = Application.new
-application.run
